7fb69f65679360a3dc2417101109ec2af5e60353
Wouter <wouter.vermeulen@student.uclouvain.be>
2024-01-23 20:53:04 +0100

    Improve comments

--- fastapi_custom.py
+++ fastapi_custom.py
@@ -5,6 +5,14 @@ from fastapi import FastAPI
 from fastapi.routing import APIRoute
 
 
+########################################################################################################################
+
+# Source: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+ALL_HTTP_METHODS = ["CONNECT", "DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT", "TRACE"]
+
+########################################################################################################################
+
+
 def use_route_names_as_operation_ids(app: FastAPI) -> None:
     """
     Simplify operation IDs so that generated API clients have simpler function
@@ -22,20 +30,27 @@ def use_route_names_as_operation_ids(app: FastAPI) -> None:
 
 ########################################################################################################################
 
+
 def hide_422(func: Callable) -> Callable:
-    """Parsing error from pydantic
-    Author: Wouter Vermeulen 2024-01-19"""
+    """
+    Indicate to `hide_default_responses` that the '422 parsing error' from pydantic should be hidden in the openapi
+    documentation for the function passed as `func` (this function is meant to be used as a decorator: `@hide_422`.
+
+    Author: Wouter Vermeulen
+    2024-01-19
+    """
     func.__hide_422__ = True
     return func
 
 
 def hide_default_responses(app: FastAPI) -> None:
     """
-    Hide the responses as indicated with the decorators above.
+    Hide the responses as indicated with the function attributes `__hide_{status_code}__`.
 
-    Slighly based on [this github comment](https://github.com/tiangolo/fastapi/issues/497#issuecomment-546752110).
+    Slighly based on: https://github.com/tiangolo/fastapi/issues/497#issuecomment-546752110.
 
-    Author: Wouter Vermeulen 2024-01-19
+    Author: Wouter Vermeulen
+    2024-01-19
     """
 
     def delete_response(route, paths, response: str):
@@ -53,7 +68,6 @@ def hide_default_responses(app: FastAPI) -> None:
         if not isinstance(route, APIRoute):
             continue
 
-        for response_status_code in [422]:
+        for response_status_code in [422]:  # Extend to other status codes here
             if getattr(route.endpoint, f"__hide_{response_status_code}__", None):
                 delete_response(route, paths, str(response_status_code))
-
