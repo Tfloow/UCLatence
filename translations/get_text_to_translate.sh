7b2cacce5737a224a75b88f8a32196410a5db355
Tfloow <88899420+Tfloow@users.noreply.github.com>
2024-02-11 12:31:18 +0100

    Added info section and better item name

--- translations/get_text_to_translate.sh
+++ translations/get_text_to_translate.sh
@@ -1,5 +1,5 @@
 pybabel extract -F babel.cfg -k _ -o to_translate.pot ..
 
-pybabel update -i to_translate.pot -d . -l fr
+pybabel update -i to_translate.pot -d . -l fr # For French language, change fr by the two letter of your own language you want to update
 
 pybabel compile -d .
